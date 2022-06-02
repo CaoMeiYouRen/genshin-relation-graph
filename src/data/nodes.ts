493e5867d0c472d607009f90ee0d5d7a42a036cf
CaoMeiYouRen <996881204@qq.com>
2022-06-02 19:27:46 +0800

    feat: 新增 夜兰、神里绫人、八重神子 语音

--- src/data/nodes.ts
+++ src/data/nodes.ts
@@ -69,27 +69,46 @@ export const nodes: PeopleNode[] = [
         country: '璃月',
     },
     {
+        name: '鹿野院平藏',
+        stars: 4,
+        weaponType: '法器',
+        elementAttribute: '风',
+        sex: '男',
+        country: '稻妻',
+    },
+    {
+        name: '久岐忍',
+        stars: 4,
+        weaponType: '单手剑',
+        elementAttribute: '雷',
+        sex: '女',
+        country: '稻妻',
+    },
+    // 以上人物还未正式实装，人物关系未知
+    {
+        name: '夜兰',
+        stars: 5,
+        weaponType: '弓',
+        elementAttribute: '水',
+        sex: '女',
+        country: '璃月',
+    },
+    {
         name: '神里绫人',
+        stars: 5,
+        weaponType: '单手剑',
+        elementAttribute: '水',
         sex: '男',
         country: '稻妻',
     },
     {
         name: '八重神子',
+        stars: 5,
+        weaponType: '法器',
         elementAttribute: '雷',
         sex: '女',
         country: '稻妻',
     },
-    {
-        name: '久岐忍',
-        sex: '女',
-        country: '稻妻',
-    },
-    {
-        name: '鹿野院平藏',
-        sex: '男',
-        country: '稻妻',
-    },
-    // 以上人物还未正式实装，人物关系未知
     {
         name: '云堇',
         stars: 4,
